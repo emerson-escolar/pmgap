2b9351c4b89919cfcf6bf5f4243f4aa60f078c3c
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2020-06-30 15:27:47 +0900

    improve some documentation commgrid(repn)

--- lib/commgrid.gd
+++ lib/commgrid.gd
@@ -6,13 +6,18 @@
 #! @Returns a commutative grid path algebra
 DeclareGlobalFunction("CommGridPathAlgebra");
 
-#!
+#! @Arguments A
+#! @Returns true or false
+#! @Description
+#! Tells you if A is commutative grid path algebra or not.
 DeclareProperty("IsCommGridPathAlgebra", IsPathAlgebra);
 
+#! @Arguments A
 #! @Description
-#! gets the number of rows in the underlying commutative grid.
+#! Gets the number of rows in the underlying commutative grid of A.
 DeclareAttribute("NumCommGridRows", IsCommGridPathAlgebra);
 
+#! @Arguments A
 #! @Description
-#! gets the number of columns in the underlying commutative grid.
+#! gets the number of columns in the underlying commutative grid of A.
 DeclareAttribute("NumCommGridColumns", IsCommGridPathAlgebra);
--- lib/commgridrepn.gd
+++ lib/commgridrepn.gd
@@ -1,22 +1,22 @@
 #! @Chapter Persistence modules (representations)
 
-
-
-
 #! @Section Persistence modules over commutative grids
 
 #! @Arguments filename
 #! @Returns commutative grid representation
 #! @Description
-#! Reads a CommGridRepn from a json file
+#! Reads a CommGridRepn from a json file.
 DeclareGlobalFunction("JsonFileToCommGridRepn");
 
 
 #! @Arguments json stream
 #! @Returns commutative grid representation
 #! @Description
-#! Reads a CommGridRepn from a json stream
+#! Reads a CommGridRepn from a json stream.
 DeclareGlobalFunction("JsonToCommGridRepn");
 
-#!
+#! @Arguments V
+#! @Returns true or false
+#! @Description
+#! Tells you if V is commutative grid representation or not.
 DeclareProperty("IsCommGridRepn", IsPathAlgebraMatModule);
