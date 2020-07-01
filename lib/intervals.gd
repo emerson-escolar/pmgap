4a00b0d610421a550cbae441adf4b8fd1bf374ba
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2020-07-01 18:24:03 +0900

    add documentation for intervals

--- lib/intervals.gd
+++ lib/intervals.gd
@@ -1,9 +1,26 @@
+#! @Chapter Persistence modules (representations)
 
+#! @Section Intervals
 
+#! @Arguments A
+#! @Returns Record of dimension vectors of intervals
+#! @Description
+#! Computes the dimension vectors, organized by "height",
+#! of the interval representations of the given
+#! commutative grid path algebra A. Here, the "height" of
+#! an interval is defined to be the number of rows of the
+#! commutative grid its support occupies.
 #!
+#! The return value is a GAP Record, with heights 1, 2, ...
+#! as the record names, and Lists of
+#! dimension vectors (also Lists) as record components.
 DeclareAttribute("IntervalDimVecs", IsCommGridPathAlgebra);
 
-#!
+#! @Arguments A, dimension_vector_of_interval
+#! @Returns CommGridRepn corresponding to the interval
+#! @Description
+#! This operation builds the representation of A corresponding
+#! to the dimension vector of an interval.
 DeclareOperation("CommGridInterval", [IsCommGridPathAlgebra, IsCollection]);
 
 #!
