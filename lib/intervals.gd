b45ce292ad688320d16e813bc26c51c65a22e1ca
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2020-07-01 18:56:51 +0900

    improve interalrepn docu

--- lib/intervals.gd
+++ lib/intervals.gd
@@ -16,12 +16,21 @@
 #! dimension vectors (also Lists) as record components.
 DeclareAttribute("IntervalDimVecs", IsCommGridPathAlgebra);
 
-#! @Arguments A, dimension_vector_of_interval
+# --------------------------------------------------
+#! @BeginGroup IntervalRepn
+#! @GroupTitle Interval representations
+#! @Arguments commutative_grid_path_algebra, dimension_vector
 #! @Returns interval representation
+
 #! @Description
 #! This operation builds the representation of A corresponding
 #! to the dimension vector of an interval.
 DeclareOperation("IntervalRepn", [IsCommGridPathAlgebra, IsCollection]);
+
+#! @Arguments equioriented_an_path_algebra, dimension_vector
+DeclareOperation("IntervalRepn", [IsEquiorientedAnPathAlgebra, IsCollection]);
+#! @EndGroup
+# --------------------------------------------------
 
 #!
 DeclareProperty("IsCommGridInterval", IsCommGridRepn);
