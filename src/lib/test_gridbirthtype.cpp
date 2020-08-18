ce66f9cf1bc6e750a65290b0d369365c84f8690e
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2020-08-18 19:26:41 +0900

    more tests for gridbirthtype

--- src/lib/test_gridbirthtype.cpp
+++ src/lib/test_gridbirthtype.cpp
@@ -11,7 +11,7 @@ TEST_CASE("invalid birth types",
 }
 
 
-TEST_CASE("Birth types",
+TEST_CASE("Birth types - comparisons",
           "[gridbirthtype]") {
   CHECK(BT(1,1) <= BT(2,3));
   CHECK(BT(2,3) <= BT(2,3));
@@ -19,10 +19,23 @@ TEST_CASE("Birth types",
   CHECK(BT(2,3) >= BT(1,1));
   CHECK(BT(2,3) >= BT(2,3));
 
-
   CHECK(BT(2,3) < BT(2,4));
   CHECK(BT(2,3) < BT(3,3));
   CHECK(BT(2,4) > BT(2,3));
   CHECK(BT(3,3) > BT(2,3));
+
+  CHECK_FALSE(BT(2,3) < BT(2,3));
+  CHECK_FALSE(BT(2,3) > BT(2,3));
+
+  CHECK(BT(4,4) == BT(4,4));
+  CHECK(BT(2,4) != BT(4,4));
+  CHECK(BT(4,2) != BT(4,4));
+}
+
+TEST_CASE("Birth types",
+          "[gridbirthtype]") {
+  BT bt(1,2);
+  CHECK(bt.row() == 1);
+  CHECK(bt.col() == 2);
 
 }
