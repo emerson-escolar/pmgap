7ff5ec82d4d9b60fa41825b1d9fd103dbcd1bc93
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2021-05-27 18:04:54 +0900

    work on documentation

--- lib/intervals.gd
+++ lib/intervals.gd
@@ -2,54 +2,42 @@
 
 #! @Section Interval formats
 
-# --------------------------------------------------
-#! @Arguments dim_vec, n_rows, n_cols
-#! @Description
-#! Converts the dimension vector (dim_vec) of an interval representation over
-#! an n_rows x n_cols comm_grid_path_algebra to the row-wise birth-death
-#! format.
+#! Two ways of specifying interval representations are provided: the dimension vector format
+#! and the row-wise birth-death format.
 #!
+#! <List>
+#! <Item>
 #! In the row-wise birth-death format, an interval is a list
-#! with the ith entry given as:
-#! a) a list with two elements [birth_i, death_i] corresponding to the
-#!    endpoints of the ith row, if the ith row is non-empty, or
-#! b) false, if the ith row is empty.
+#! with the $i$th entry given as:
+#! a) a list with two elements <C>[birth_i, death_i]</C> corresponding to the
+#!    endpoints of the $i$th row, if the ith row is non-empty, or
+#! b) <C>false</C>, if the ith row is empty.
+#! </Item>
 #!
-#! Note that the interval representation nor the comm_grid_path_algebra
-#! need not be explicitly constructed.
-#! @Returns row-wise birth-death
-DeclareOperation("IntervalDimVecToRowWiseBD", [IsList, IsInt, IsInt]);
+#! <Item>
+#! In the dimension vector format, an interval is given as a list of dimensions (either $1$ or $0$) of the
+#! vector spaces at the vertices of the underlying quiver.
+#! Note that this depends on the vertices of <Ref Func="CommGridPathAlgebra"/> being ordered in a very specific way!
+#! In terms of (row,col) indices, it is assumed to be ordered as:
+#! (1,1) (1,2) ... (1,n_cols) (2,1) (2,2) ... (2,n_cols) (n_rows,1) (n_rows,2) ... (n_rows,n_cols).
+#! </Item>
+#! </List>
+#!
+#! There are additional requirements for being an interval.
+#! See Proposition 4.1 and the definition of "staircases" in <Cite Key="asashiba2018interval"/> for more details.
+
+
+# These are checked by the functions
+# <Ref Oper="CheckRowWiseBD"/>, <Ref Oper="CheckCommGridIntervalDimVec"/>, <Ref Oper="CheckAnIntervalDimVec"/>.
 
 # --------------------------------------------------
 #! @Arguments rwbd, n_rows, n_cols
 #! @Description
-#! Converts the row-wise-birth-death list (rwbd) of an interval representation over
-#! an n_rows x n_cols comm_grid_path_algebra to its dimension vector.
+#! Checks whether or not the input row-wise-birth-death list (<A>rwbd</A>) represents
+#! an interval representation over an <A>n_rows</A> $\times$ <A>n_cols</A>
+#! equioriented commutative grid path algebra.
 #!
-#! In the row-wise birth-death format, an interval is a list
-#! with the ith entry given as:
-#! a) a list with two elements [birth_i, death_i] corresponding to the
-#!    endpoints of the ith row, if the ith row is non-empty, or
-#! b) false, if the ith row is empty.
-#!
-#! Note that the interval representation nor the comm_grid_path_algebra
-#! need not be explicitly constructed.
-#! @Returns dimension vector
-DeclareOperation("RowWiseBDToIntervalDimVec", [IsList, IsInt, IsInt]);
-
-# --------------------------------------------------
-#! @Arguments rwbd, n_rows, n_cols
-#! @Description
-#! Checks whether or not the input row-wise-birth-death list (rwbd) represents
-#! an interval representation over an n_rows x n_cols comm_grid_path_algebra.
-#!
-#! In the row-wise birth-death format, an interval is a list
-#! with the ith entry given as:
-#! a) a list with two elements [birth_i, death_i] corresponding to the
-#!    endpoints of the ith row, if the ith row is non-empty, or
-#! b) false, if the ith row is empty.
-#!
-#! Note that the interval representation nor the comm_grid_path_algebra
+#! Note that the interval representation nor the path algebra
 #! need not be explicitly constructed.
 #! @Returns true or false
 DeclareOperation("CheckRowWiseBD", [IsList, IsInt, IsInt]);
@@ -57,24 +45,49 @@ DeclareOperation("CheckRowWiseBD", [IsList, IsInt, IsInt]);
 # --------------------------------------------------
 #! @Arguments dim_vec, n_rows, n_cols
 #! @Description
-#! Checks whether or not the input dimension vector (dim_vec) represents
-#! an interval representation over an n_rows x n_cols comm_grid_path_algebra.
+#! Checks whether or not the input dimension vector (<A>dim_vec</A>) represents
+#! an interval representation over an <A>n_rows</A> $\times$ <A>n_cols</A>
+#! equioriented commutative grid path algebra.
 #!
-#! Note that the interval representation nor the comm_grid_path_algebra
-#! need not be explicitly constructed.
+#! Note that the interval representation or the path algebra
+#! do not need to be explicitly constructed.
 #! @Returns true or false
 DeclareOperation("CheckCommGridIntervalDimVec", [IsList, IsInt, IsInt]);
 
 #! @Arguments dim_vec, n
 #! @Description
-#! Checks whether or not the input dimension vector (dim_vec) represents
-#! an interval representation over a equioriented A_n path algebra with
-#! n vertices.
+#! Checks whether or not the input dimension vector (<A>dim_vec</A>) represents
+#! an interval representation over a equioriented $A_n$ path algebra with
+#! <A>n</A> vertices.
 #!
-#! Note that the interval representation nor the path_algebra
-#! need not be explicitly constructed.
+#! Note that the interval representation or the path algebra
+#! do not need to be explicitly constructed.
 #! @Returns true or false
 DeclareOperation("CheckAnIntervalDimVec", [IsList, IsInt]);
+
+
+# --------------------------------------------------
+#! @Arguments dim_vec, n_rows, n_cols
+#! @Description
+#! Converts the dimension vector (<A>dim_vec</A>) of an interval representation over
+#! an <A>n_rows</A> $\times$ <A>n_cols</A> equioriented commutative grid path algebra
+#! to the row-wise birth-death format.
+#!
+#! Note that the interval representation nor the path algebra
+#! need not be explicitly constructed.
+#! @Returns row-wise birth-death
+DeclareOperation("IntervalDimVecToRowWiseBD", [IsList, IsInt, IsInt]);
+
+# --------------------------------------------------
+#! @Arguments rwbd, n_rows, n_cols
+#! @Description
+#! Converts the row-wise-birth-death list (<A>rwbd</A>) of an interval representation over
+#! an <A>n_rows</A> $\times$ <A>n_cols</A> equioriented commutative grid path algebra to its dimension vector.
+#!
+#! Note that the interval representation nor the path algebra
+#! need not be explicitly constructed.
+#! @Returns dimension vector
+DeclareOperation("RowWiseBDToIntervalDimVec", [IsList, IsInt, IsInt]);
 # --------------------------------------------------
 
 #! @Section Generating intervals
@@ -86,7 +99,7 @@ DeclareOperation("CheckAnIntervalDimVec", [IsList, IsInt]);
 #! @Description
 #! Computes the dimension vectors, organized by "height",
 #! of the interval representations of the given
-#! equioriented An or commutative grid path algebra.
+#! <A>commutative_grid_path_algebra</A> or <A>equioriented_an_path_algebra</A>.
 #! Here, the "height" of
 #! an interval is defined to be the number of rows of the
 #! commutative grid its support occupies.
@@ -107,15 +120,17 @@ DeclareAttribute("IntervalDimVecs", IsEquiorientedAnPathAlgebra);
 # --------------------------------------------------
 #! @BeginGroup IntervalRepn
 #! @GroupTitle Interval representation
-#! @Arguments commutative_grid_path_algebra, dimension_vector
-#! @Returns interval representation
 
 #! @Description
-#! This operation builds the representation of A corresponding
-#! to the dimension vector of an interval.
+#! This operation builds the interval representation of
+#! a <A>commutative_grid_path_algebra</A> or <A>equioriented_an_path_algebra</A>
+#! with dimension vector given by <A>dim_vec</A>.
+#! @Returns interval representation
+
+#! @Arguments commutative_grid_path_algebra, dim_vec
 DeclareOperation("IntervalRepn", [IsCommGridPathAlgebra, IsCollection]);
 
-#! @Arguments equioriented_an_path_algebra, dimension_vector
+#! @Arguments equioriented_an_path_algebra, dim_vec
 DeclareOperation("IntervalRepn", [IsEquiorientedAnPathAlgebra, IsCollection]);
 #! @EndGroup
 # --------------------------------------------------
@@ -128,12 +143,12 @@ DeclareOperation("IntervalRepn", [IsEquiorientedAnPathAlgebra, IsCollection]);
 #! @Description
 #! Computes the interval representations, organized by "height",
 #! of the interval representations of the given
-#! equioriented An or commutative grid path algebra.
+#! <A>commutative_grid_path_algebra</A> or <A>equioriented_an_path_algebra</A>.
 #! Here, the "height" of
 #! an interval is defined to be the number of rows of the
 #! commutative grid its support occupies.
 #!
-#! The return value is a GAP Record, with heights 1, 2, ...
+#! The return value is a &GAP; Record, with heights 1, 2, ...
 #! as the record names, and Lists of
 #! interval representations as record components.
 #! @Returns Record of interval representations
@@ -149,7 +164,7 @@ DeclareAttribute("IntervalRepns", IsEquiorientedAnPathAlgebra);
 #! @Arguments V
 #! @Returns true or false
 #! @Description
-#! Tells you if V is an interval or not.
+#! Tells you if <A>V</A> is an interval or not.
 DeclareProperty("IsCommGridInterval", IsCommGridRepn);
 #!
 InstallTrueMethod(IsIndecomposableModule, IsCommGridInterval);
@@ -157,7 +172,7 @@ InstallTrueMethod(IsIndecomposableModule, IsCommGridInterval);
 #! @Arguments V
 #! @Returns true or false
 #! @Description
-#! Tells you if V is an interval or not.
+#! Tells you if <A>V</A> is an interval or not.
 DeclareProperty("IsEquiorientedAnInterval", IsEquiorientedAnRepn);
 #!
 InstallTrueMethod(IsIndecomposableModule, IsEquiorientedAnInterval);
@@ -170,24 +185,30 @@ InstallTrueMethod(IsIndecomposableModule, IsEquiorientedAnInterval);
 #! @Arguments V
 #! @Returns list
 #! @Description
-#! Computes the largest interval-decomposable summand of V.
-#! The output is a list containing lists [interval_repn, multiplicity]
-#! giving the decomposition of the largest interval-decomposable summand of V.
-#! Warning: non-obvious cases involve much computation
+#! Computes the largest interval-decomposable summand of <A>V</A>.
+#! The output is a list containing lists [interval_indecomposable, multiplicity]
+#! giving the decomposition of the largest interval-decomposable summand of <A>V</A>.
+#!
+#! Uses the algorithm of <Cite Key="asashiba2018interval"/>.
+#! Warning: non-obvious cases involve much computation.
 DeclareAttribute("IntervalPart", IsCommGridRepn);
 
 #! @Arguments V
 #! @Returns dimension vector
 #! @Description
-#! Computes the dimension vector of the largest interval-decomposable summand of V
-#! Warning: non-obvious cases involve much computation
+#! Computes the dimension vector of the largest interval-decomposable summand of <A>V</A>
+#!
+#! Uses the algorithm of <Cite Key="asashiba2018interval"/>.
+#! Warning: non-obvious cases involve much computation.
 DeclareOperation("IntervalPartDimVec", [IsCommGridRepn]);
 
 #! @Arguments V
 #! @Returns true or false
 #! @Description
-#! Tells you if V is interval decomposable or not.
-#! Warning: non-obvious cases involve much computation
+#! Tells you if <A>V</A> is interval decomposable or not.
+#!
+#! Uses the algorithm of <Cite Key="asashiba2018interval"/>.
+#! Warning: non-obvious cases involve much computation.
 DeclareProperty("IsIntervalDecomposable", IsCommGridRepn);
 
 InstallTrueMethod(IsIntervalDecomposable, IsEquiorientedAnRepn);
